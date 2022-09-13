7047d13594cf0772a487036929283d876aa888f4
Jorge Alvarez-Jarreta <jalvarez@ebi.ac.uk>
2022-09-13 20:59:49 +0100

    update PhyloAssemble module to new code standards

--- MEvoLib/PhyloAssemble/_Consense.py
+++ MEvoLib/PhyloAssemble/_Consense.py
@@ -1,141 +1,99 @@
-#-------------------------------------------------------------------------------
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
 #
-#   MEvoLib  Copyright (C) 2016  J. Alvarez-Jarreta
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#   This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-#   This is free software, and you are welcome to redistribute it under certain
-#   conditions; type `show c' for details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#-------------------------------------------------------------------------------
-# File :  _Consense.py
-# Last version :  v1.00 ( 02/Feb/2016 )
-# Description :  MEvoLib's variables library functions to ease the usage of
-#       Consense from PHYLIP
-#       (<http://evolution.genetics.washington.edu/phylip.html>).
-#-------------------------------------------------------------------------------
-# Historical report :
-#
-#   DATE :  02/Feb/2016
-#   VERSION :  v1.00
-#   AUTHOR(s) :  J. Alvarez-Jarreta
-#
-#-------------------------------------------------------------------------------
-
-from __future__ import absolute_import
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""MEvoLib's variables library functions to ease the usage of Consense from PHYLIP."""
 
 import os
 import tempfile
+from typing import List
 
 from Bio import Phylo
 
 from MEvoLib._utils import get_abspath
 
 
-#-------------------------------------------------------------------------------
-
 SPRT_INFILE_FORMATS = ['newick']
-
 KEYWORDS = {'default': ['R', '2', 'Y']} # majority rule consensus
 
 
-#-------------------------------------------------------------------------------
+def gen_args(args: str, infile_path: str, outfile: str) -> List:
+    """Returns the list of arguments generated from `args` and the infile path requested to call Consense.
 
-def gen_args ( args, infile_path, outfile ) :
+    Args:
+        args: Keyword or arguments to use in the call of Consense, excluding infile and outfile arguments.
+        infile_path: Input alignment file path.
+        outfile: Consensus tree output file.
+
     """
-    Return the argument list generated from 'args' and the infile path
-    requested.
-
-    Arguments :
-        args  ( string )
-            Keyword or arguments to use in the call of Consense, excluding
-            infile and outfile arguments.
-        infile_path  ( string )
-            Input alignment file path.
-        outfile  ( string )
-            Consensus tree output file.
-
-    Returns :
-        list
-            List of arguments (excluding binary file) to call Consense.
-    """
-    if ( outfile ) :
+    if outfile:
         outfile_path = get_abspath(outfile)
-    else :
-        # Output files will be saved in temporary files to retrieve the
-        # consensus tree
-        outfile_path = os.path.join(tempfile.gettempdir(),
-                                    tempfile.gettempprefix() + \
-                                        next(tempfile._get_candidate_names()))
+    else:
+        # Output files will be saved in temporary files to retrieve the consensus tree
+        outfile_path = os.path.join(
+            tempfile.gettempdir(),
+            tempfile.gettempprefix() + next(tempfile._get_candidate_names())
+        )
     # Create full command line list
     argument_list = [infile_path, outfile_path]
-    return ( argument_list )
+    return argument_list
 
+def gen_stdin_content(args: str) -> str:
+    """Returns the standard input content generated from `args`.
 
+    Args:
+        args: Keyword or arguments to use in the call of Consense, excluding infile and outfile arguments.
+            If `args` is not a keyword, the second character will be used as separator of the different
+            arguments.
 
-def gen_stdin_content ( args ) :
     """
-    Arguments :
-        args  ( string )
-            Keyword or arguments to use in the call of Consense, excluding
-            infile and outfile arguments. If 'args' is not a keyword, the second
-            character will be used as separator of the different arguments.
-
-    Returns :
-        string
-            Standard input content generated from 'args'.
-    """
-    if ( args in KEYWORDS ) :
+    if args in KEYWORDS:
         options = KEYWORDS[args]
-    else : # args not in KEYWORDS
-        options = [opt  for opt in args.split(args[1])]
-    # If the output file already exists, overwritte it
+    else:
+        options = [opt for opt in args.split(args[1])]
+    # If the output file already exists, overwrite it
     options.append('R')
     stdin_content = '\n'.join(options) + '\n'
-    return ( stdin_content )
+    return stdin_content
 
 
+def get_results(command: List) -> Bio.Phylo.BaseTree:
+    """Returns consensus tree from the files generated during the execution of `command`.
 
-def get_results ( command ) :
-    """
-    Extract resultant consensus tree from the files generated during the
-    execution of 'command'.
+    Args:
+        command: Consense's command line executed.
 
-    Arguments :
-        command  ( list )
-            Consense's command line executed.
+    Raises:
+        IOError: If the consensus tool did not generate a consensus tree (indicated by user's options).
 
-    Returns :
-        Bio.Phylo.BaseTree
-            Resultant consensus tree.
-
-    Raises :
-        IOError
-            If the consensus tool didn't generate a consensus tree (indicated by
-            user's options/arguments).
     """
     outfile_path = command[2]
-    try :
+    try:
         consensus_tree = Phylo.read(outfile_path, 'newick')
-    except IOError as e :
+    except IOError as e:
         cleanup(command)
-        message = 'The given arguments don\'t generate a consensus tree file'
-        raise IOError(message)
-    else :
-        return ( consensus_tree )
+        raise IOError('The given arguments did not generate a consensus tree file')
+    else:
+        return consensus_tree
 
 
+def cleanup(command: List) -> None:
+    """Removes the temporary file created (if any) in `gen_args()` function.
 
-def cleanup ( command ) :
-    """
-    Remove the temporary file created (if any) in gen_args() function.
+    Args:
+        command: Consense's command line executed.
 
-    Arguments :
-        command  ( list )
-            Consense's command line executed.
     """
     logfile_path = get_abspath(command[2])
-    if ( os.path.dirname(logfile_path) == tempfile.gettempdir() ) :
+    if os.path.dirname(logfile_path) == tempfile.gettempdir():
         os.remove(logfile_path)
-
-
-#-------------------------------------------------------------------------------
--- MEvoLib/PhyloAssemble/__init__.py
+++ MEvoLib/PhyloAssemble/__init__.py
@@ -1,147 +1,94 @@
-#-------------------------------------------------------------------------------
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
 #
-#   MEvoLib  Copyright (C) 2016  J. Alvarez-Jarreta
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#   This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-#   This is free software, and you are welcome to redistribute it under certain
-#   conditions; type `show c' for details.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#-------------------------------------------------------------------------------
-# File :  __init__.py
-# Last version :  v1.10 ( 16/Jul/2016 )
-# Description :  Functions aimed to provide an easy interface to handle
-#       different phylogenetic assembly tools (supertrees, consensus trees,
-#       ...).
-#-------------------------------------------------------------------------------
-# Historical report :
-#
-#   DATE :  16/Jul/2016
-#   VERSION :  v1.10
-#   AUTHOR(s) :  J. Alvarez-Jarreta
-#   CHANGES :  * Added get_tools() method to have an easy access to all the
-#                  available supertee and consensus tree tools.
-#
-#   DATE :  12/Feb/2016
-#   VERSION :  v1.00
-#   AUTHOR(s) :  J. Alvarez-Jarreta
-#
-#-------------------------------------------------------------------------------
-
-from __future__ import absolute_import
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""APIs to handle different phylogenetic assembly tools (supertrees, consensus trees, ...)."""
 
 import os
 import tempfile
 import subprocess
 
-from . import _Consense
-
 from Bio import Phylo
 
 from MEvoLib._utils import get_abspath
-from MEvoLib._py3k import viewkeys, viewitems, DEVNULL, StringIO
+from MEvoLib.PhyloAssemble import Consense
 
 
-#-------------------------------------------------------------------------------
-
-#_STREE_TOOL_TO_LIB = { 'superfine': _SuperFine }
 _STREE_TOOL_TO_LIB = { }
-
-_CONS_TOOL_TO_LIB = { 'consense': _Consense }
-
-
-#-------------------------------------------------------------------------------
-
-def get_tools ( ) :
-    """
-    Returns :
-        dict
-            Dictionary of supertree and consensus tree tools included in the
-            current version of MEvoLib.
-    """
-    return ( dict([('supertree', list(viewkeys(_STREE_TOOL_TO_LIB))),
-                   ('consensus', list(viewkeys(_CONS_TOOL_TO_LIB)))]) )
+_CONS_TOOL_TO_LIB = { 'consense': Consense }
 
 
+def get_tools() -> dict:
+    """Returns a dict of supertree and consensus tree tools included in the current version of MEvoLib."""
+    return dict([
+               ('supertree', list(_STREE_TOOL_TO_LIB.keys())),
+               ('consensus', list(_CONS_TOOL_TO_LIB.keys()))
+           ])
 
-def get_keywords ( tool ) :
-    """
-    Arguments :
-        tool  ( string )
-            Name of the supertree or consensus tool.
 
-    Returns :
-        dict
-            Dictionary containing the keywords and their corresponding
-            arguments.
+def get_keywords(tool: str) -> dict:
+    """Returns a dictionary containing the keywords and their corresponding arguments.
 
-    Raises :
-        ValueError
-            If the tool introduced isn't included in MEvoLib.PhyloAssemble.
+    Args:
+        tool: Name of the supertree or consensus tool.
+
+    Raises:
+        ValueError: If the tool introduced is not included in ``MEvoLib.PhyloAssemble``.
     """
     tool = tool.lower()
-    #tool_lib_keys = viewkeys(_STREE_TOOL_TO_LIB) | viewkeys(_CONS_TOOL_TO_LIB)
-    tool_lib_keys = viewkeys(_CONS_TOOL_TO_LIB)
-    if ( tool not in tool_lib_keys ) :
-        message = 'The tool "{}" isn\'t included in ' \
-                  'MEvoLib.PhyloAssemble'.format(tool)
-        raise ValueError(message)
-    # else : # tool in tool_lib_keys
+    tool_lib_keys = _STREE_TOOL_TO_LIB.keys() | _CONS_TOOL_TO_LIB.keys()
+    if tool not in tool_lib_keys:
+        raise ValueError(f'The tool "{tool}" is not included in MEvoLib.PhyloAssemble')
     keyword_dict = {}
-#    if ( tool in _STREE_TOOL_TO_LIB ) :
-#        tool_lib_dict = _STREE_TOOL_TO_LIB
-#    else : # tool in _CONS_TOOL_TO_LIB
-    if ( tool in _CONS_TOOL_TO_LIB ) :
+    if tool in _STREE_TOOL_TO_LIB:
+        tool_lib_dict = _STREE_TOOL_TO_LIB
+    else:
         tool_lib_dict = _CONS_TOOL_TO_LIB
-    for key, value in iter(viewitems(tool_lib_dict[tool].KEYWORDS)) :
+    for key, value in iter(tool_lib_dict[tool].KEYWORDS.items()) :
         keyword_dict[key] = ' '.join(value)
-    return ( keyword_dict )
+    return keyword_dict
 
 
-
-
-def get_consensus_tree ( binary, infile, infile_format, args = 'default',
-                         outfile = None, outfile_format = 'newick' ) :
+def get_consensus_tree(binary: str, infile: str, infile_format: str, args: str = 'default',
+                       outfile: str = None, outfile_format: str = 'newick' ) -> Bio.Phylo.BaseTree:
     """
     Calculate the consensus tree of the input trees file with the given
     arguments. The resultant consensus tree is returned as a Bio.Phylo.BaseTree
-    object and saved in the ouput file (if provided). If 'infile' or 'outfile'
+    object and saved in the output file (if provided). If 'infile' or 'outfile'
     contain a relative path, the current working directory will be used to get
     the absolute path. If the output file already exists, the old file will be
     overwritten without any warning.
 
-    Arguments :
-        binary  ( string )
-            Name or path of the consensus tool.
-        infile  ( string )
-            Input phylogenetic trees file.
-        infile_format  ( string )
-            Input file format.
-        args  ( Optional[string] )
-            Keyword or arguments to use in the call of the consensus tool,
+    Args:
+        binary: Name or path of the consensus tool.
+        infile: Input phylogenetic trees file.
+        infile_format: Input file format.
+        args: Keyword or arguments to use in the call of the consensus tool,
             excluding infile and outfile arguments. By default, 'default'
             arguments are used.
             * For Consense, the second character will be used as separator of
             the different arguments. 
-        outfile  ( Optional[string] )
-            Consensus tree output file.
-        outfile_format  ( Optional[string] )
-            Output file format. By default, NEWICK format.
+        outfile: Consensus tree output file.
+        outfile_format: Output file format. By default, NEWICK format.
 
-    Returns :
+    Returns:
         Bio.Phylo.BaseTree
             Resultant consensus tree.
 
-    Raises :
-        ValueError
-            If the tool introduced isn't included in MEvoLib.
-        IOError
-            If the input path or the input file provided doesn't exist.
-        RuntimeError
-            If the call to the phylogenetic inference tool command raises an
-            exception.
-        IOError
-            If the consensus tool didn't generate a consensus tree (indicated by
-            user's options/arguments).
+    Raises:
+        ValueError: If the tool introduced isn't included in MEvoLib.
+        IOError: If the input path or the input file provided doesn't exist.
+        IOError: If the consensus tool did not generate a consensus tree.
 
     * The input file format must be supported by Bio.Phylo.
     * The output file format must be supported by Bio.Phylo.
@@ -149,47 +96,34 @@ def get_consensus_tree ( binary, infile, infile_format, args = 'default',
     # Get the variables associated with the given consensus tool
     bin_path, bin_name = os.path.split(binary)
     bin_name = bin_name.lower()
-    if ( bin_name in _CONS_TOOL_TO_LIB ) :
-        tool_lib = _CONS_TOOL_TO_LIB[bin_name]
-        sprt_infile_formats = tool_lib.SPRT_INFILE_FORMATS
-        gen_args = tool_lib.gen_args
-        gen_stdin_content = tool_lib.gen_stdin_content
-        get_results = tool_lib.get_results
-        cleanup = tool_lib.cleanup
-    else : # bin_name not in _CONS_TOOL_TO_LIB
-        message = 'The consensus tool "{}" isn\'t included in ' \
-                  'MEvoLib.PhyloAssemble'.format(bin_name)
-        raise ValueError(message)
+    if bin_name not in _CONS_TOOL_TO_LIB:
+        raise ValueError(f'The consensus tool "{bin_name}" is not included in MEvoLib.PhyloAssemble')
+    tool_lib = _CONS_TOOL_TO_LIB[bin_name]
+    sprt_infile_formats = tool_lib.SPRT_INFILE_FORMATS
+    gen_args = tool_lib.gen_args
+    gen_stdin_content = tool_lib.gen_stdin_content
+    get_results = tool_lib.get_results
+    cleanup = tool_lib.cleanup
     # Get the command line to run in order to get the consensus tree
     infile_path = get_abspath(infile)
     # If the input file format is not supported by the consensus tool, convert
     # it to a temporary supported file
-    if ( infile_format.lower() not in sprt_infile_formats ) :
+    if infile_format.lower() not in sprt_infile_formats:
         tmpfile = tempfile.NamedTemporaryFile()
-        Phylo.convert(infile_path, infile_format, tmpfile.name,
-                      sprt_infile_formats[0])
+        Phylo.convert(infile_path, infile_format, tmpfile.name, sprt_infile_formats[0])
         infile_path = tmpfile.name
     # Create full command line list
     command = [binary] + gen_args(args, infile_path, outfile)
     # Generate the standard input file content
     stdin_content = gen_stdin_content(args)
     # Create the input file with the given options
-    with tempfile.NamedTemporaryFile(mode='w+') as stdin_file :
+    with tempfile.NamedTemporaryFile(mode='w+') as stdin_file:
         stdin_file.write(stdin_content)
         stdin_file.seek(0)
-        # Run the consensus process handling any Runtime exception
-        try :
-            subprocess.check_call(command, stdin=stdin_file, stdout=DEVNULL,
-                                  stderr=DEVNULL, universal_newlines=True)
-        except subprocess.CalledProcessError as e :
-            cleanup(command)
-            message = 'Running "{}" raised an exception'.format(' '.join(e.cmd))
-            raise RuntimeError(message)
-        else :
-            consensus_tree = get_results(command)
-            cleanup(command)
-            # Return the resultant consensus tree as a Bio.Phylo.BaseTree object
-            return ( consensus_tree )
-
-
-#-------------------------------------------------------------------------------
+        # Run the consensus process
+        subprocess.run(command, stdin=stdin_file, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+                       universal_newlines=True, check=True)
+        consensus_tree = get_results(command)
+        cleanup(command)
+        # Return the resultant consensus tree as a ``Bio.Phylo.BaseTree`` object
+        return consensus_tree
