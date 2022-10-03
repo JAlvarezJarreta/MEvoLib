bec819e2d20b52c80a7dda35bfcbdf29b3e9d768
sansanpre <100790997+sansanpre@users.noreply.github.com>
2022-10-03 11:23:29 +0200

    Update mevolib/fetch/BioSeqs.py
    Co-authored-by: J. Alvarez-Jarreta <jalvarez@ebi.ac.uk>

--- mevolib/fetch/BioSeqs.py
+++ mevolib/fetch/BioSeqs.py
@@ -351,7 +351,8 @@ class BioSeqs:
 
 
     def join (self, new_bioseqs: BioSeqs):
-        """Join the information of the BioSeqs object with another BioSeqs object.
+        """Joins the information of the BioSeqs object with another BioSeqs object.
+
         For any matching sequence, the new information (from the external BioSeqs object) will replace the 
         existing one.
 
