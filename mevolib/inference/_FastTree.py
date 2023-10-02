b079738ede9ce236c8055fd46ecc0e43df630128
SMoragon <145195231+SMoragon@users.noreply.github.com>
2023-10-02 20:15:35 +0200

    Update mevolib/inference/_FastTree.py
    Co-authored-by: Jose Ignacio Requeno <ppnacho85@gmail.com>

--- mevolib/inference/_FastTree.py
+++ mevolib/inference/_FastTree.py
@@ -134,7 +134,7 @@ def get_results (command: list, output: str) -> Bio.Phylo.BaseTree:
 
 
 
-def cleanup ( command: list ):
+def cleanup (command: list):
     """
     Remove the temporary files and directories created (if any) in gen_args()
     function.
